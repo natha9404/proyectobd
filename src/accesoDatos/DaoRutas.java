c79603c8668e0413526fb41b92eb4492578d0aaf
Nathalia Bedoya Hurtado <natha9404@gmail.com>
2016-03-01 01:38:18 -0500

    acssac

--- src/accesoDatos/DaoRutas.java
+++ src/accesoDatos/DaoRutas.java
@@ -159,33 +159,7 @@ public class DaoRutas {
         return lista;
     }
     
-     public ArrayList listar_estaciones2(){
-        String sql_select = "SELECT nombre_estacion FROM Estacion";
-        
-        ArrayList<String> lista = new ArrayList<>();
-        
-        try {
-            Connection conn = fachada.conectar();
-            Statement sentencia = conn.createStatement();
-            ResultSet tabla = sentencia.executeQuery(sql_select);
-            System.out.println("se conecto");
-            //
-            while (tabla.next()) {
-                lista.add(tabla.getString(1));
-                
-            }
-            
-            conn.close();
-            System.out.println("Conexion cerrada");
-            return lista;
-            
-        } catch (SQLException e) {
-            System.out.println(e);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return lista;
-    }
+    
     
     public ArrayList estaciones_eliminar(String nombre){
         String sql_select = "SELECT nombre_estacion FROM Ruta_Estacion WHERE nombre_ruta = '" + nombre + "'";
